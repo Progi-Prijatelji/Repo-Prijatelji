85719dcb6a4a7de9d23bdb76e94c2baca8a6e7e7
niksamujo1 <nm55788@fer.hr>
2025-10-22 13:51:57 +0200

    Preuređenje login page-a
    Malo sam promjenio boje, malo repozicionirao i uredio komponente. Trebalo bi još vizualno poboljšati i implementirati uspješnu registraciju i prijavu.

--- FlipMemo/src/App.jsx
+++ FlipMemo/src/App.jsx
@@ -1,28 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/FlipMemo__Logo.png'
 
 function App() {
- 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Handle login logic here
+  }
 
   return (
-    <div className='container'>
-      <div className='title part'>
-        <h1>Dobro došli u aplikaciju Flip Memo!!!!!</h1>
+    <>
+      <img src={logo} alt="Flip Memo Logo" className="page-logo" />
+      <div className='container'>
+        
+        <div className='title part'>
+          <h1>Flip Memo</h1>
+        </div>
+        <div className='login part'>
+          <form action="" onSubmit={handleSubmit} className='login-form'>
+            <label id = "welcome-label">Dobrodosli</label>
+            <label id = "login-label">Prijava</label>
+            <div className="login-inputs">
+              
+              <input 
+                type="text" 
+                id="mail" 
+                name="mail"
+                value = {email} 
+                onChange = {(e) => setEmail(e.target.value)}
+                placeholder="Email" required/>
+              
+              <label htmlFor="mail">Email: </label>
+
+            </div>
+            <div className="login-inputs">
+              <input 
+                type="password" 
+                id="password" 
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Lozinka" required/>
+
+              <label htmlFor="password">Lozinka: </label>
+            </div>
+            <input type="submit" value="Prijavi se" className='button' />
+          </form> 
+          <div className="noAcc">
+            <p>Nemaš račun?</p>
+            <button className='button'>Registriraj se</button>
+
+          </div>
+        </div>
       </div>
-      <div className='login part'>
-        <form action="/action_page.php">
-          <label htmlFor="mail">email:</label>
-          <input type="text" id="mail" name="mail" />
-          <label htmlFor="password">lozinka:</label>
-          <input type="text" id="password" name="password"  />
-          <input type="submit" value="Submit" className='button' />
-        </form> 
-        <p>Nemaš račun?</p>
-        <button className='button'>registriraj se</button>
-      </div>
-    </div>
+    </>
   )
 }
 
