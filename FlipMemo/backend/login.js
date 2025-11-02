66812b260f2b6051ecfcd32557ab2b49099e60f6
emazebic <ez56227@fer.hr>
2025-11-02 16:12:00 +0100

    Update login.js - database

--- FlipMemo/backend/login.js
+++ FlipMemo/backend/login.js
@@ -1,40 +1,60 @@
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
+const {Client} = require('pg');
+const client = new Client ({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "bazepodataka",
+    database: "FlipMemo"
 });
+client.connect();
 
-con.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL!");
-});
+client.query(`Select * from login`, (err, res) => {
+    if (!err) {
+        console.log(res.rows);
+    }
+    else {
+        console.log(err.message);
+    }
+    client.end;
+})
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
-  const sql = "SELECT COUNT(*) AS count FROM login WHERE email = ? AND password = ?";
-  
-  con.query(sql, [email, password], (err, result) => {
+  client.query(`SELECT COUNT(*) AS count FROM login WHERE email=${email} AND password=${password}`, (err, result) => {
     if (err) {
-      console.error(err);
+      console.error(err.message);
       return res.status(500).json({ success: false });
     }
-
+    console.log(result);
     if (result.length > 0) {
       return res.json({ success: true });
     } else {
       return res.json({ success: false });
     }
   });
+
+  // const sql = "SELECT COUNT(*) AS count FROM login WHERE email = ? AND password = ?";
+  
+  // con.query(sql, [email, password], (err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return res.status(500).json({ success: false });
+  //   }
+
+  //   if (result.length > 0) {
+  //     return res.json({ success: true });
+  //   } else {
+  //     return res.json({ success: false });
+  //   }
+  // });
 });
 
 app.post('/signup', (req, res) => {
