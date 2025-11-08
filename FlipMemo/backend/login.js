f69358e67e6d73076b7f3448fc950a36306c4327
evakitonic <142660998+evakitonic@users.noreply.github.com>
2025-11-08 18:38:31 +0100

    Update login.js

--- FlipMemo/backend/login.js
+++ FlipMemo/backend/login.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const {Client} = require('pg');
+const {Client} = require('pg'); // napravljeno za posebnu bazu mora se promijeniti
 const client = new Client ({
     host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "bazepodataka",
+    user: "myuser",
+    port: 5433,
+    password: "mysecretpwd",
     database: "FlipMemo"
 });
 client.connect();
@@ -22,23 +22,25 @@ client.query(`Select * from login`, (err, res) => {
     else {
         console.log(err.message);
     }
-    client.end;
 })
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
-  client.query(`SELECT COUNT(*) AS count FROM login WHERE email=${email} AND password=${password}`, (err, result) => {
-    if (err) {
-      console.error(err.message);
-      return res.status(500).json({ success: false });
-    }
-    console.log(result);
-    if (result.length > 0) {
-      return res.json({ success: true });
-    } else {
-      return res.json({ success: false });
-    }
+    const query = `SELECT COUNT(*) AS count FROM login WHERE email=$1 AND password=$2`;
+    client.query(query, [email, password], (err, result) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ success: false });
+        }
+
+        const count = parseInt(result.rows[0].count);
+        if (count > 0) {
+            return res.json({ success: true });
+        } else {
+            return res.json({ success: false });
+        }
+    });
   });
 
   // const sql = "SELECT COUNT(*) AS count FROM login WHERE email = ? AND password = ?";
@@ -55,25 +57,20 @@ app.post('/login', (req, res) => {
   //     return res.json({ success: false });
   //   }
   // });
-});
 
 app.post('/signup', (req, res) => {
   const { email, password } = req.body;
 
   const sql = "insert into mydb values(?,?)";
   
-  con.query(sql, [email, password], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ success: false });
-    }
-
-    if (result.length > 0) {
-      return res.json({ success: true });
-    } else {
-      return res.json({ success: false });
-    }
-  });
+  const query = `INSERT INTO login (email, password) VALUES ($1, $2) RETURNING *`;
+    client.query(query, [email, password], (err, result) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ success: false });
+        }
+        return res.json({ success: true, user: result.rows[0] });
+    });
 });
 
 const PORT = 8080;
