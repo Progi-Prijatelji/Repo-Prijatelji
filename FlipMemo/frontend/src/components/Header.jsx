29a043ce7d3e4f392c290add184bb2ac3a9c6360
niksamujo1 <nm55788@fer.hr>
2026-01-14 15:17:18 +0100

    Dodao dropdown menu s logout opcijom

--- FlipMemo/frontend/src/components/Header.jsx
+++ FlipMemo/frontend/src/components/Header.jsx
@@ -2,10 +2,19 @@ import React, { useState } from 'react';
 import '../css/header.css'
 import logo from '../assets/FlipMemo__Logo.png';
 import { Link } from 'react-router-dom';
-import { Settings } from 'lucide-react';
+import { Settings, Menu, LogOut } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const [language, setLanguage] = useState('engleski')
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        alert("Odjavljeni ste!");
+        localStorage.removeItem("token");
+        navigate("/");
+    }
 
     return(
         
@@ -16,18 +25,32 @@ const Header = () => {
                 src={logo} alt="Flip Memo Logo" 
                 className="page-logo" /></Link>
             <h1>FlipMemo</h1>
-            <div className="setting">
+            <div className="dropdown">
+                <button 
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                    className="dropdown-button">
+                    <Menu size={28}/>
+                </button>
 
-                <Link to="/home/settings">
-                    <Settings className="setting-icon"
-                                color="black"
-                                size="30"/>
-                </Link>
+                <div className={`dropdown-menu ${isDropdownOpen ? 'open' : ''}`}>
+                    <Link 
+                        to='/home/settings'
+                        className="dropdown-item"
+                        onClick={() => setIsDropdownOpen(false)}>
+                        <Settings size={16} style={{marginRight: '8px'}}/>
+                        <p>Postavke</p>
+                    </Link>
+                    <button 
+                        className="dropdown-item"
+                        onClick={handleLogout}>
+                        <LogOut size={16} style={{marginRight: '8px'}}/>
+                        <p>Odjava</p>
+                    </button>
+                </div>
+
             </div>
-        </header>
+      </header>
         
     )
 }
-
-
 export default Header;
