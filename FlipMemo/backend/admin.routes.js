a1734eced934e772863c52a64b00d8831c756019
emazebic <ez56227@fer.hr>
2026-01-16 13:48:57 +0100

    popravak prebacivanja apija na backend

--- FlipMemo/backend/admin.routes.js
+++ FlipMemo/backend/admin.routes.js
@@ -9,6 +9,10 @@ const DATABASE_USER = process.env.DATABASE_USER;
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 
+const apiLanguageIds = new Map();
+apiLanguageIds.set('afrikaans', '1'); apiLanguageIds.set('arapski', '2'); apiLanguageIds.set('bengalski', '4'); apiLanguageIds.set('bugarski', '6'); apiLanguageIds.set('katalonski', '7'); apiLanguageIds.set('češki', '8'); apiLanguageIds.set('danski', '9'); apiLanguageIds.set('nizozemski', '11'); apiLanguageIds.set('engleski', '22'); apiLanguageIds.set('filipnski', '23'); apiLanguageIds.set('finski', '25'); apiLanguageIds.set('francuski', '28'); apiLanguageIds.set('njemački', '30'); apiLanguageIds.set('grčki', '32'); apiLanguageIds.set('gudžaratski', '33'); apiLanguageIds.set('hindi', '35'); apiLanguageIds.set('mađarski', '37'); apiLanguageIds.set('islandski', '38'); apiLanguageIds.set('indonezijski', '39'); apiLanguageIds.set('talijanski', '41'); apiLanguageIds.set('kanadski', '43'); apiLanguageIds.set('korejski', '45'); apiLanguageIds.set('latvijski', '47'); apiLanguageIds.set('malajski', '48'); apiLanguageIds.set('malajalam', '50'); apiLanguageIds.set('norveški', '52'); apiLanguageIds.set('poljski', '54'); apiLanguageIds.set('portugalski', '56'); apiLanguageIds.set('pandžapski', '58'); apiLanguageIds.set('rumunjski', '60'); apiLanguageIds.set('ruski', '61'); apiLanguageIds.set('srpski', '63'); apiLanguageIds.set('slovački', '64'); apiLanguageIds.set('španjolski', '65'); apiLanguageIds.set('švedski', '69'); apiLanguageIds.set('tamilski', '71'); apiLanguageIds.set('telugu', '74'); apiLanguageIds.set('tajlandski', '75'); apiLanguageIds.set('turski', '76'); apiLanguageIds.set('ukrajinski', '78'); apiLanguageIds.set('vijetnamski', '80');
+    
+
 const client = new Client({
   host: DATABASE_HOSTNAME,
   user: DATABASE_USER,
@@ -230,8 +234,8 @@ router.post('/addWord', verifyToken, verifyAdmin, async (req, res) => {
 
     let audioFile = "";
     let postId = "";
-    const lang = languages.find(l => l.langid===Number(wordLangID));
-    if (apiLanguageIds.get(lang.langname)) {
+    const lang = await client.query(`SELECT langname FROM languages where langid = $1 `, [langid]);
+    if (apiLanguageIds.get(lang.rows[0].langname)) {
         const audioResults = await fetch("https://thefluentme.p.rapidapi.com/post", {
             method: "POST",
             headers: { 
@@ -239,7 +243,7 @@ router.post('/addWord', verifyToken, verifyAdmin, async (req, res) => {
                 'x-rapidapi-host': 'thefluentme.p.rapidapi.com',
                 'Content-Type': 'application/json'
           },
-            body: JSON.stringify({post_language_id: apiLanguageIds.get(lang.langname),  post_title: word, post_content: word})
+            body: JSON.stringify({post_language_id: apiLanguageIds.get(lang.rows[0].langname),  post_title: word, post_content: word})
         });
         const audioData = await audioResults.json();
         audioFile = audioData.ai_reading;
